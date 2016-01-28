e846674c276d57633e2093a78f6b1e0df775b3bd
Brice VICO <brice.vico@sfr.fr>
2016-01-28 19:25:25 +0100

    t

--- Angular/app/scripts/controllers/main.js
+++ Angular/app/scripts/controllers/main.js
@@ -5,23 +5,23 @@
 
 angular.module('myApp')
     .controller('mainCtrl', function ($scope, $http, $location) {
-
         $scope.submit = function () {
-
             $scope.ajaxLoader = true;
-
-          $http
+            $scope.rcv = null;
+            $http
               .get('http://localhost:8000/' + $scope.terme)
               .success(function (data) {
-                 $scope.rcv =  data;
+                  $scope.rcv =  data;
                   $scope.ajaxLoader = false;
-                  $(document).ready( function () {
+//                  $(document).ready( function () {
                       $('body').find('#tableEntrants').DataTable();
-                  });
+                      $('body').find('#tableSortants').DataTable();
+  //                });
               })
               .error(function (data) {
                   $scope.rcv = data;
                   $scope.ajaxLoader = false;
+                  alert('Aucune correspondance pour ce mot.');
               });
         };
     });
